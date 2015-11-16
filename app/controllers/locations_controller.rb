e9b0c58bd5d7b920a35089bdc5a9530ab65a2073
James Moon <mooniker@gmail.com>
2015-11-16 17:27:04 -0500

    Add basic controller stuff and views to get and show locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,26 @@
 class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+  end
+
+  def new # get user's location, mapped to from/where
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.save
+    redirect_to @location
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+  
+  private
+    def location_params
+      params.require(:location).permit(:lat, :lon)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'from/where' => 'locations#new'
+  root 'welcome#index'
+
+  resources :locations
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
