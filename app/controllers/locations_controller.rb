27b05d2471091644f81da7d4cb0d7c57b21a957b
James Moon <mooniker@gmail.com>
2015-11-17 22:46:08 -0500

    Refactor unused location model to see if that makes Heroku happy

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,10 +19,13 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find(params[:id])
-
     # @stop_attributes = ['StopID', 'Name', 'Lon', 'Lat', 'Routes']
-    # @stops = Wmata.new(ENV['wmata_api_key']).get_bus_stops(@location.lat, @location.lon, 300)
-    @stops = Wmata.new(ENV['wmata_api_key']).get_departure_times_for_nearby_bus_stops(@location.lat, @location.lon, 300)
+    stops = Wmata.new(ENV['wmata_api_key']).get_bus_stops(@location.lat, @location.lon, 300)
+    puts stops.inspect
+    @predictions = []
+    stops.each do |stop|
+      @predictions << Wmata.new(ENV['wmata_api_key']).get_predictions(stop['StopID'])
+    end # why does this nest like this?
 
   end
 
