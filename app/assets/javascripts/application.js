e0514df2c53acf3535e9662f1d5b996c6cef5afe
James Moon <mooniker@gmail.com>
2015-11-19 13:22:43 -0500

    Move JS to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,113 +16,117 @@
 //= require_tree .
 
 
-function geoFindMe() {
-  var output = document.getElementById("out");
+// function geoFindMe() {
+//   var output = document.getElementById("out");
+//
+//   if (!navigator.geolocation){
+//     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+//     return;
+//   }
+//
+//   function success(position) {
+//     var latitude  = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//
+//     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+//
+//     var img = new Image();
+//     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+//
+//     output.appendChild(img);
+//   };
+//
+//   function error() {
+//     output.innerHTML = "Unable to retrieve your location";
+//   };
+//
+//   output.innerHTML = "<p>Locating…</p>";
+//
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
+// defaults
+var latitude = 38.8898; // U.S. Capitol
+var longitude = -77.0091;
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+// constants
+var token = 'pk.eyJ1IjoibW9vbmlrZXIiLCJhIjoiY2loNHkwMmUwMHp1Znc5bTVxZGptZ3d1eSJ9.IjtdkC-4egUXjw39mKShgA';
+// var address_query = "https://api.mapbox.com/geocoding/v5/{dataset}/{query}.json?proximity={longitude},{latitude}&access_token={token}";
+//
+// function make_address_query_url( query ) {
+// // "https://api.mapbox.com/geocoding/v5/{dataset}/{query}.json?proximity={longitude},{latitude}&access_token={token}";
+//   var prefix = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+//   return prefix + query + ".json?proximity=" + longitude + "," + latitude + "&access_token=" + token;
+// }
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+// function set_lon_lat_from_address() {
+//
+//   var jsonhttp = new XMLHttpRequest();
+//
+//   jsonhttp.onreadystatechange = function() {
+//     if (jsonhttp.readyState == 4 && jsonhttp.status == 200) {
+//         var mapbox_address_JSON = JSON.parse(jsonhttp.responseText);
+//         var latlon = mapbox_address_JSON.features[0].center;
+//         latitude = latlon[0];
+//         longitude = latlon[1];
+//         console.log("Global lon/lat set to", longitude, latitude);
+//     }
+//     jsonhttp.open("GET", make_address_query_url("1600+pennsylvania+ave+nw"), true);
+//     jsonhttp.send();
+//   };
+// }
 
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+var map;
 
-    output.appendChild(img);
-  };
+function make_map (lat, lon) {
+  var default_zoom = 15;
+  L.mapbox.accessToken = token;
+  map = L.mapbox.map('map', 'mapbox.pirates')
+      .setView([lat, lon], default_zoom);
 
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
+  // L.marker is a low-level marker constructor in Leaflet.
+  L.marker([lat, lon], {
+      icon: L.mapbox.marker.icon({
+          'marker-size': 'large',
+          'marker-symbol': 'building',
+          'marker-color': '#fa0'
+      })
+  }).addTo(map);
 
-  output.innerHTML = "<p>Locating…</p>";
+  // for each bus stop on dashboard, display it
+  var stops = [];
+  $('.bus_stop_data').each( function (index, value) {
+    var stop = $(this).children();
+    var s = {};
+    s.name = stop.eq(0).text();
+    s.id = stop.eq(1).text();
+    s.lat = stop.eq(2).text();
+    s.lon = stop.eq(3).text();
+    stops.push(s);
+  });
 
-  navigator.geolocation.getCurrentPosition(success, error);
-}
-
-$(document).ready(function () {
-
-  var latitude = 38.8898;
-  var longitude = 77.0091;
-  var token = 'pk.eyJ1IjoibW9vbmlrZXIiLCJhIjoiY2loNHkwMmUwMHp1Znc5bTVxZGptZ3d1eSJ9.IjtdkC-4egUXjw39mKShgA';
-  var address_query = "https://api.mapbox.com/geocoding/v5/{dataset}/{query}.json?proximity={longitude},{latitude}&access_token={token}";
-
-  function make_address_query_url( query ) {
-  // "https://api.mapbox.com/geocoding/v5/{dataset}/{query}.json?proximity={longitude},{latitude}&access_token={token}";
-    var prefix = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
-    return prefix + query + ".json?proximity=" + longitude + "," + latitude + "&access_token=" + token;
-  }
-
-  function set_lon_lat_from_address() {
-
-    var jsonhttp = new XMLHttpRequest();
-
-    jsonhttp.onreadystatechange = function() {
-      if (jsonhttp.readyState == 4 && jsonhttp.status == 200) {
-          var mapbox_address_JSON = JSON.parse(jsonhttp.responseText);
-          var latlon = mapbox_address_JSON.features[0].center;
-          latitude = latlon[0];
-          longitude = latlon[1];
-          console.log("Global lon/lat set to", longitude, latitude);
-      }
-      jsonhttp.open("GET", make_address_query_url("1600+pennsylvania+ave+nw"), true);
-      jsonhttp.send();
-    };
-  }
-
-  function makeMap (lat, lon) {
-    L.mapbox.accessToken = token;
-    var map = L.mapbox.map('map', 'mapbox.pirates')
-        .setView([lat, lon], 16);
-
-    // L.marker is a low-level marker constructor in Leaflet.
-    L.marker([lat, lon], {
+  stops.forEach ( function (stop) {
+    L.marker([stop.lat, stop.lon], {
         icon: L.mapbox.marker.icon({
             'marker-size': 'large',
-            'marker-symbol': 'building',
+            'marker-symbol': 'bus',
             'marker-color': '#fa0'
         })
     }).addTo(map);
+  });
+}
 
-    // for each bus stop on dashboard, display it
-    var stops = [];
-    $('.bus_stop_data').each( function (index, value) {
-      var stop = $(this).children();
-      var s = {};
-      s.name = stop.eq(0).text();
-      s.id = stop.eq(1).text();
-      s.lat = stop.eq(2).text();
-      s.lon = stop.eq(3).text();
-      stops.push(s);
-    });
+$( document ).ready(function() {
 
-    stops.forEach ( function (stop) {
-      L.marker([stop.lat, stop.lon], {
-          icon: L.mapbox.marker.icon({
-              'marker-size': 'large',
-              'marker-symbol': 'bus',
-              'marker-color': '#fa0'
-          })
-      }).addTo(map);
-    });
-  }
-
-  if ( $('#lat').length != 0 && $('lon') != 0 ) {
-    var lat = parseFloat( $('#lat').text() );
-    var lon = parseFloat( $('#lon').text() );
-    makeMap( lat, lon );
-    // console.log('Map generated');
-  } else if ( $('#dashboard_lat').length == 1 && $('#dashboard_lon').length == 1 ) {
+  if ( $('#lat').length !== 0 && $('#lon').length !== 0 ) {
+    make_map( parseFloat( $('#lat').text() ), parseFloat( $('#lon').text() ) );
+  } else if ( $('#dashboard_lat').val().length > 0 && $('#dashboard_lon').val().length > 0 ) {
     var lat = parseFloat( $('#dashboard_lat').val() );
-    var lon = parseFloat( $('#dashboard_lon').val() );
-    // console.log('Map generated from form');
-    makeMap( lat, lon );
+    var lon =  parseFloat( $('#dashboard_lon').val() );
+    make_map( lat, lon );
+    console.log( "Map set from dashboard form to", lat, lon);
   } else {
-    makeMap( parseFloat(latitude), parseFloat(longitude) );
+    make_map( latitude, longitude );
+    console.log( "Map by default set to", latitude, longitude );
   }
 });
